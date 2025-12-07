7ec0692ae9c687487626f8db09e37b04eed691b4
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-12-07 15:41:02 +0100

    Update partOne.py

--- 2025/004/partOne.py
+++ 2025/004/partOne.py
@@ -8,143 +8,23 @@ neigbor_rolls = 0
 
 for line in range(len(lines)):
     for character in range(len(lines[line])):
-        ############################################
-        if lines[line][character]  == "@":
-            all_of_rolls +=1 
-        ############################################
         
         neigbor_rolls = 0
-        try:
-            if lines[line][character]  == "@":
-                #oben
-                if lines[line-1][character-1]  == "@":
-                    neigbor_rolls +=1
-                if lines[line-1][character]  == "@":
-                    neigbor_rolls +=1
-                if lines[line-1][character+1]  == "@":
-                    neigbor_rolls +=1
-                #nebenan
-                if lines[line][character-1]  == "@":
-                    neigbor_rolls +=1
-                if lines[line][character+1]  == "@":
-                    neigbor_rolls +=1
-                #darunter
-                if lines[line+1][character-1]  == "@":
-                    neigbor_rolls +=1
-                if lines[line+1][character]  == "@":
-                    neigbor_rolls +=1
-                if lines[line+1][character+1]  == "@":
-                    neigbor_rolls +=1
-                if neigbor_rolls < 4:
-                    rolls_of_paper +=1
-        except:
-            None    
+        
+        if lines[line][character]  == ".":
+            continue
 
-
-        ############################################
-#ecken
-if lines[0][0]  == "@":    
-    rolls_of_paper +=1
-if lines[0][len(lines[0])-1]  == "@":    
-    rolls_of_paper +=1
-if lines[len(lines)-1][0]  == "@":    
-    rolls_of_paper +=1
-if lines[len(lines)-1][len(lines[len(lines)-1])-1]   == "@":    
-    rolls_of_paper +=1
-
-#oberste Zeile
-for i in range(1, len(lines[0])-1):
-    neigbor_rolls = 0
-    #nebenan
-    if lines[0][i-1]  == "@":
-        neigbor_rolls +=1
-    if lines[0][i+1]  == "@":
-        neigbor_rolls +=1
-    #darunter
-    if lines[1][i-1]  == "@":
-        neigbor_rolls +=1
-    if lines[1][i]  == "@":
-        neigbor_rolls +=1
-    if lines[1][i+1]  == "@":
-        neigbor_rolls +=1
-    if neigbor_rolls < 4:
-        rolls_of_paper +=1
-
-#unterste Zeile
-for i in range(1, len(lines[0])-1):
-    neigbor_rolls = 0
-    #nebenan
-    if lines[len(lines)-1][i-1]  == "@":
-        neigbor_rolls +=1
-    if lines[len(lines)-1][i+1]  == "@":
-        neigbor_rolls +=1
-    #darüber
-    if lines[len(lines)-2][i-1]  == "@":
-        neigbor_rolls +=1
-    if lines[len(lines)-2][i]  == "@":
-        neigbor_rolls +=1
-    if lines[len(lines)-2][i+1]  == "@":
-        neigbor_rolls +=1
-    if neigbor_rolls < 4:
-        rolls_of_paper +=1
-
-
-#linkeste Spalte
-for i in range(1, len(lines)-1):
-    neigbor_rolls = 0
-    #oben oder unten
-    if lines[i-1][0]  == "@":
-        neigbor_rolls +=1
-    if lines[i+1][0]  == "@":
-        neigbor_rolls +=1
-    #rechts daneben
-    if lines[i-1][1]  == "@":
-        neigbor_rolls +=1
-    if lines[i][1]  == "@":
-        neigbor_rolls +=1
-    if lines[i+1][1]  == "@":
-        neigbor_rolls +=1
-    if neigbor_rolls < 4:
-        rolls_of_paper +=1
-
-#rechtest Spalte
-for i in range(1, len(lines)-1):
-    neigbor_rolls = 0
-    #oben oder unten
-    if lines[i-1][len(lines[i])-1]  == "@":
-        neigbor_rolls +=1
-    if lines[i+1][len(lines[i])-1]  == "@":
-        neigbor_rolls +=1
-    #links daneben
-    if lines[i-1][len(lines[i])-2]  == "@":
-        neigbor_rolls +=1
-    if lines[i][len(lines[i])-2]  == "@":
-        neigbor_rolls +=1
-    if lines[i+1][len(lines[i])-2]  == "@":
-        neigbor_rolls +=1
-    if neigbor_rolls < 4:
-        rolls_of_paper +=1
-
-
-"""
-        neigbor_rolls = 0
-        try:
-            if lines[line][character]  == "@":
-                if lines[line-1][character]  == ".":
-                    rolls_of_paper +=1
+        for dr in [-1, 0, 1]:
+            for dc in [-1, 0, 1]:
+                if dr == 0 and dc == 0:
                     continue
-                if lines[line+1][character]  == ".":
-                    rolls_of_paper +=1
-                    continue
-                if lines[line][character-1]  == ".":
-                    rolls_of_paper +=1
-                    continue
-                if lines[line][character+1]  == ".":
-                    rolls_of_paper +=1
-                    continue
-        except:
-            None
-"""
+                if line+dr in range(0,len(lines)) and character+dc in range(0, len(lines[0])):
+                    if lines[line+dr][character+dc] == "@":
+                        neigbor_rolls += 1
 
-#print(all_of_rolls)
+        
+        if neigbor_rolls <4:
+            rolls_of_paper +=1
+
+
 print(rolls_of_paper)
