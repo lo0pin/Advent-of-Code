6c62e84bd570e455cdf08bdb469267780c0de5da
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-12-07 12:25:58 +0100

    Implement logic to find and sum maximum integers
    Iterate through lines to find maximum integers and sum results.

--- 2025/003/003partTwo.py
+++ 2025/003/003partTwo.py
@@ -1,1 +1,40 @@
+from input import *
+
+list_of_lines = data.splitlines()
+#print(len(list_of_lines)) # 200 list_of_lines
+# 100 numbers per line
+
+final_result = 0
+
+for line in list_of_lines: #über jede Zeile itierieren
+    integers = []
+    indices = []
+
+    for j in range(11,-1,-1):
+        try:
+            start = indices[-1]
+        except:
+            start = 0
+        current_maximum = 0
+        #print(start)
+        for i in range((start),len(line)):
+            #print (current_maximum)
+            if int(line[i]) > current_maximum and i < len(line)-j:
+                current_maximum = int(line[i])
+                current_max_idx = i 
+        integers.append(str(current_maximum))
+        indices.append(current_max_idx+1)
+
+
+    result = "".join(integers)
+    final_result += int(result)
+    print(result)
+    print(indices)
+
+  
+
+print (final_result)
+
+
+
 
