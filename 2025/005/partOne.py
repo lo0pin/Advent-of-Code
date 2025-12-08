0fa7eb3f97bb9d246332526f4271964e2ea0c000
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-12-08 12:25:07 +0100

    Add fresh food counter logic in partOne.py
    Count fresh foods based on given ID ranges.

--- 2025/005/partOne.py
+++ 2025/005/partOne.py
@@ -1,1 +1,21 @@
+from input import *
 
+ids = data.splitlines()
+
+foods = data2.splitlines()
+fresh_counter = 0
+
+for food in foods:
+    food = int(food)
+    for id in ids:
+        start = int(id.split("-")[0])
+        end = int(id.split("-")[1])
+        if food >= start and food <= end:
+            fresh_counter += 1
+            break
+        else:
+            continue
+        
+
+
+print (fresh_counter)
