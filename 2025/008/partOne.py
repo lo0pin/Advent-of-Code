718663c2010fd100c20e5a2064d050c77cb55d3a
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-12-09 23:53:06 +0100

    Improve distance calculation and add progress tracking
    Refactor distance calculation and add progress output.

--- 2025/008/partOne.py
+++ 2025/008/partOne.py
@@ -1,16 +1,16 @@
 from input import *
 import math
 
-print(data)
-
 lines = data.splitlines()
-
-#circuits = []
-#all_points_n_distances = []
+mittelwert_distance =0
+counter_distance = 0
+shortest_distance = 99999999999999999999
+idx_shortest_dist = 0
+circuits = []
+all_points_n_distances = []
 distances_container = []
 
-for i in range(len(lines)):
-    
+for i in range(len(lines)):    
     for j in range(i+1, len(lines)):
         first_point = lines[i].split(",")
         secon_point = lines[j].split(",")
@@ -19,12 +19,20 @@ for i in range(len(lines)):
             vector.append(int(first_point[k])-int(secon_point[k]))
         distance = math.sqrt(pow(vector[0],2)+pow(vector[1],2)+pow(vector[2],2))
         distances_container.append([".".join(first_point),".".join(secon_point),distance])
-    print(".", end="")
+
+
 
 short_list = []
 current_shortest = 0
-for j in range(len(distances_container)-1):
-    shortest_distance = 99999999999999999999
+
+
+HOW_MANY_VALUES = 1000
+
+#for j in range(len(distances_container)-1):
+for j in range(HOW_MANY_VALUES):
+    shortest_distance = 99999999999999999999  
+    if j % (HOW_MANY_VALUES/10) == 0:
+        print (j/HOW_MANY_VALUES*100, " %")
 
     for i in range(len(distances_container)):
         if distances_container[i][2]< shortest_distance and distances_container[i][2]>current_shortest:
@@ -40,3 +48,8 @@ for j in range(len(distances_container)-1):
 print(distances_container[short_list[0]][2])
 print(distances_container[short_list[1]][2])
 print(distances_container[short_list[2]][2])
+
+
+
+
+
