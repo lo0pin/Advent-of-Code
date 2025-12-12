bf7025da240303668a574a3e27e44669592b8165
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-12-12 21:44:28 +0100

    Simplify start value adjustment logic
    Refactor start value adjustment to use modulo operation.

--- 2025/001/partOne.py
+++ 2025/001/partOne.py
@@ -8,10 +8,7 @@ for i in range(len(instructions)):
     parameter = 1 if instructions[i][0].lower() == "r" else -1
 
     start += int(instructions[i][1:])*parameter
-    while start >= 100:
-        start -= 100
-    while start < 0:
-        start +=100
+    start = start%100
     counter = counter+1 if start == 0 else counter
 
 print(counter)  
