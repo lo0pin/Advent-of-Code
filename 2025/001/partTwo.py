62357146487d7f74218025cddf5d17f7cd3b1350
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-12-12 21:33:51 +0100

    Update partTwo.py

--- 2025/001/partTwo.py
+++ 2025/001/partTwo.py
@@ -1,1 +1,20 @@
+from input import *
+
+instructions = data.strip().splitlines()
+
+start = 50
+counter = 0
+for i in range(len(instructions)):
+    parameter = 1 if instructions[i][0].lower() == "r" else -1
+
+    for j in range(0, int(instructions[i][1:])):
+        start += 1*parameter
+        match start:
+            case -1:
+                start=99
+            case 100:
+                start=0
+        counter = counter+1 if start == 0 else counter 
+
+print(counter)
 
