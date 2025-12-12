266a4bb6adbdf793179957325261a651ca32798f
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-12-12 21:42:21 +0100

    Simplify movement calculations in partOne.py
    Refactor movement logic to simplify direction handling.

--- 2025/001/partOne.py
+++ 2025/001/partOne.py
@@ -1,29 +1,17 @@
 from input import *
 
 instructions = data.strip().splitlines()
-#print(instructions)
-
-directions = []
-moves = []
-for i in instructions:
-    directions.append(i[0])
-    moves.append(int(i[1:]))
-#print (moves)
 
 start = 50
 counter = 0
 for i in range(len(instructions)):
-    if directions[i].lower() == "r":
-        start+=moves[i]
-    else:
-        start-=moves[i]
+    parameter = 1 if instructions[i][0].lower() == "r" else -1
 
-    while start>= 100:
-        start-=100
+    start += int(instructions[i][1:])*parameter
+    while start >= 100:
+        start -= 100
     while start < 0:
-        start += 100
+        start +=100
+    counter = counter+1 if start == 0 else counter
 
-    if start == 0:
-        counter +=1
-
-print(counter)
+print(counter)  
