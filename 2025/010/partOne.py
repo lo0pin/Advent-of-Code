e5afa21e3fc9c806b334766eca67d29d0e4e82d4
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-12-12 14:53:53 +0100

    Refactor button logic and compute total presses
    Refactor button application logic and calculate total button presses.

--- 2025/010/partOne.py
+++ 2025/010/partOne.py
@@ -6,6 +6,40 @@ instructions_per_machine = []
 """for  machine in instructions:
     instructions_per_machine.append(machine.split(" "))"""
 
+
+
+def apply_button(state_string, button_list):
+    state_list = list(state_string)
+
+    for i in button_list:
+        if state_list[i] == ".":
+            state_list[i] = "#"
+        else:
+            state_list[i] = "." 
+    return "".join(state_list)
+
+def minimum_button_presses(zielstring, buttons):
+    start = "."*len(zielstring)
+
+    queue = []
+    queue.append((start, 0))
+
+    visited = set()
+    visited.add(start)
+
+    while queue:
+        state, dist = queue.pop(0)
+        if state == zielstring:
+            return dist
+        
+        for button in buttons:
+            new_state = apply_button(state, button)
+            if  new_state not in visited:
+                visited.add(new_state)
+                queue.append((new_state, dist+1))
+    return None
+
+
 for  machine in instructions:
     buffer = []
     for i in machine.split(" "):
@@ -34,12 +68,14 @@ for parts in instructions_per_machine:
 
     machines.append((ziel_string, buttons))
 
-def apply_button(state_string, button_list):
-    state_list = list(state_string)
+print(machines[:5])
 
-    for i in button_list:
-        if state_list[i] == ".":
-            state_list[i] = "#"
-        else:
-            state_list[i] = "." 
-    return "".join(state_list)
+button_total = 0
+
+for ziel_string_, buttons in machines:
+    presses = minimum_button_presses(ziel_string_, buttons)
+    button_total += presses
+
+print(button_total)
+
+
