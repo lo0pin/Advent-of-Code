857485767b66226e553e06149d79c0c1dbe812e3
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-12-16 16:39:33 +0100

    Add initial comments and import statement

--- 2025/006/partTwo.py
+++ 2025/006/partTwo.py
@@ -1,1 +1,34 @@
+"""
+not solved
+"""
 
+from input import *
+
+lines = data.splitlines()
+myarray = []
+newarray = []
+final_result = 0
+
+for line in lines:
+    current_line = line.split(" ")
+    myarray.append(current_line)
+
+
+for i in myarray:
+    for j in i:
+        if j != '':
+            newarray.append(j)
+
+for  i in range(int(len(newarray)/5)):
+    if (newarray[i+(int(len(newarray)/5))*4]) == "+":
+        result =int(newarray[i+(int(len(newarray)/5))*0])+int(newarray[i+(int(len(newarray)/5))*1])+int(newarray[i+(int(len(newarray)/5))*2])+int(newarray[i+(int(len(newarray)/5))*3])
+        final_result += result
+        print("yolk")
+    elif (newarray[i+(int(len(newarray)/5))*4]) == "*":
+        print("molk")
+        result =int(newarray[i+(int(len(newarray)/5))*0])*int(newarray[i+(int(len(newarray)/5))*1])*int(newarray[i+(int(len(newarray)/5))*2])*int(newarray[i+(int(len(newarray)/5))*3])
+        final_result += result
+        #print(newarray[i+(int(len(newarray)/5))*4])
+
+
+print (final_result)
