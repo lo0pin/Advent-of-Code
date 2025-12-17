63889926ac5b27f2ae82c59b3620cb1568cf6dd9
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-12-17 21:06:10 +0100

    Improve path calculations in partTwo.py
    Refactor path processing logic and add debug prints.

--- 2025/007/partTwo.py
+++ 2025/007/partTwo.py
@@ -2,9 +2,25 @@ from input import data
 
 lines = data.splitlines()
 lines = lines[2:-1:2]
+print(len(lines))
 
-paths[]
+paths = []
+for i in range(len(lines[0])):
+    paths.append(0)
+paths[70]=1
 
 for line in range(len(lines)):
-  for idx in range(len(line)):
-    if line[idx]:
+    to_add = []
+    for idx in range(len(lines[line])):
+        if lines[line][idx] == "^" and paths[idx] > 0:
+            to_add.append((idx,paths[idx]))
+        elif paths[idx] > 0:
+            paths[idx] 
+
+    for i in to_add:
+        paths[i[0]] = 0
+        paths[i[0]-1] += i[1]
+        paths[i[0]+1] += i[1]
+    
+#print(paths)
+print(sum(paths))
