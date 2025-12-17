4334b37f736afe1a93d3aeb7ff72b7bf1d9cf23d
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-12-17 18:06:53 +0100

    Update partTwo.py

--- 2025/007/partTwo.py
+++ 2025/007/partTwo.py
@@ -1,9 +1,10 @@
 from input import data
 
 lines = data.splitlines()
-
 lines = lines[2:-1:2]
 
-for i in lines:
-  print(i)
+paths[]
 
+for line in range(len(lines)):
+  for idx in range(len(line)):
+    if line[idx]:
