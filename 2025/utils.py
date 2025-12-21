ffc07f99675c9eb63b3c5c58caaa0d89cd2bc63d
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-12-21 18:31:43 +0100

    Update utils.py

--- 2025/utils.py
+++ 2025/utils.py
@@ -1,4 +1,14 @@
+"""prints the values of a given variable
+    format: 
+    debug(list=list)
+    @output:
+"""
 def debug(**kwargs):
     for k,v in kwargs.items():
         print(f"{k} = {v}\t")
     print()
+
+#einheitliches Format für  einen String
+###############CHAPTER 1################
+def ausgabe(inp):
+    print(inp.upper().center(40, "#"))
