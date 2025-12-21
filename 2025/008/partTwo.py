89aa2d2187b5ea93c79b40c5d36532a03485c01a
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-12-21 18:28:57 +0100

    Update partTwo.py

--- 2025/008/partTwo.py
+++ 2025/008/partTwo.py
@@ -5,6 +5,7 @@ lines = data.splitlines()
 circuits = []
 #all_points_n_distances = []
 distances_container = []
+first = True
 
 for i in range(len(lines)):    
     for j in range(i+1, len(lines)):
@@ -18,28 +19,65 @@ for i in range(len(lines)):
 
 
 distances_container.sort(key=lambda coord: coord[-1])
-#print(distances_container[:100])
+#print(distances_container[:1000])
 
-print("done")
+#print("done")
+#for i in distances_container[:1000]:
 for i in distances_container:
-    a = distances_container[i][0]
-    b = distances_container[i][1]
+    a = i[0]
+    b = i[1]
     break_condition = False
     merge_counter = 0
     merge_indices = []
 
+
     for j in enumerate(circuits):
 
-        #Both Points are member of the same circuit --> Continue
+        #Both Points are already member of the same circuit --> Continue
         if a in j[1] and b in j[1]:
             break_condition = True
             break
-        elif a in j[1] or b in j[1]:
+        elif a in j[1]:
             merge_counter += 1
-            merge_indices.append(j[0])
+            merge_indices.append((j[0],b))
+        elif b in j[1]:
+            merge_counter += 1
+            merge_indices.append((j[0],a))
+        if merge_counter>=2:
+            #break_condition = True
+            break
     if break_condition:
         continue
+    
+
+    #besser mit match?
+    if merge_counter >= 2:
+        circuits[merge_indices[0][0]].extend(circuits[merge_indices[1][0]])
+        del circuits[merge_indices[1][0]]
+    elif merge_counter == 1:
+        while merge_indices:   
+            temp = merge_indices.pop(0)
+            circuits[temp[0]].append(temp[1])
+    elif merge_counter == 0:
+        circuits.append([a, b])
+
+    if len(circuits) == 1:
+        if not first:
+            result = int(a.split(".")[0]) * int(b.split(".")[0]) 
+            print(result)
+        first = False
+        #print(int(a[0])*int(b[0]))
+        
 
 
+"""circuits.sort(reverse = True, key= lambda a : len(a))
+result = 1"""
 
+"""for i in range(3):
+    result *= len(circuits[i])
+    #print(len(circuits[i]))
+    #print((circuits[i]))"""
 
+"""print(len(circuits))
+
+print(result)"""
