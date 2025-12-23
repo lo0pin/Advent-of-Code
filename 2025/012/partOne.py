8d303ca97b27e61e392aea9dd1cd141a4bcfe70d
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-12-23 22:07:41 +0100

    Refactor forms list comprehension for clarity

--- 2025/012/partOne.py
+++ 2025/012/partOne.py
@@ -1,8 +1,11 @@
 from input import data, data2
 
+
+########## DATENAUFBEREITUNG ###########
+
 forms = (data.split("\n"))
 forms = [i for i in forms if i.startswith("#") or i.startswith(".")]
-forms = [[forms[i],forms[i+1], forms[i+2]] for i in range(0,len(forms),3)]
+forms = [(forms[i],forms[i+1], forms[i+2]) for i in range(0,len(forms),3)]
 
 instructions = data2.splitlines()
 for i in range(len(instructions)):
@@ -17,11 +20,10 @@ for i in range(len(instructions)):
     number = tuple(number)
     instructions[i] = (field, number)
 
-    
+"""print(instructions[:10])
+print(forms[:5])"""   
+
+############## FUNKTIONEN ##############
 
 
-
-print(instructions[:10])
-"""for i in forms:
-    print(i)"""
 
