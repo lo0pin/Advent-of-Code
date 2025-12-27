e603cf1924cc7fd94f2d3142cada1f9f38e5c3b2
Julian Kampitsch <julian.kampitsch@ms.bulme.at>
2025-12-27 15:20:48 +0100

    Add initial implementation of partTwo.py for machine processing

--- 2025/011/partTwo.py
+++ 2025/011/partTwo.py
@@ -1,1 +1,14 @@
+from input import data
+
+machines = data.splitlines()
+machineDic = {}
+
+for i in machines: 
+    left = i[0:i.index(":")]
+    right = i[i.index(":")+2:].strip().split(" ")
+    machineDic[left] = right
+
+alle_wege = []
+start, ziel = "svr", "out"
+stack = [(start, (start,))]
 
